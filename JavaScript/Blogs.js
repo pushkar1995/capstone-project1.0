d5a7973c7432bcbce377480e53d3a1769f31db38
pushkar1995 <pushkar.gautam1994@gmail.com>
2023-07-05 15:08:30 +0545

    Basic layout getting ready with functional mobilemenu and contact form validation

---
+++ JavaScript/Blogs.js
@@ -0,0 +1,8 @@
+// const jungleManiacsBlogs = [
+//     {
+//         blogTitle: 'Hiking in Raniban',
+//         blogSubDescription: 'Sunday, Sep 2, 2017',
+//         blogImage: '',
+
+//     },
+// ]
